4a4f8458a0ca2ffd60477333ae0b3e2a3322246b
Dylan Wagner <vstrideultimate@gmail.com>
2018-03-10 23:27:51 -0500

    Added .ino test file

---
+++ tests/TestDSQ/TestDSQ.ino
@@ -0,0 +1,42 @@
+/**
+ * File: TestDSQ.ino
+ * Organization: RIT Space Exploration
+ * Author: Dylan Wagner (drw6528@rit.edu)
+ * Description:
+ *      Test program for DSQ
+ */
+
+#include <DSQ.h>
+
+DSQ dsq;
+
+void testFunc(){
+
+    Serial.println("Hello World!");
+
+    dsq.add_routine(0, 0, testFunc);
+}
+
+void testFunc3(){
+    Serial.println("BAR!");
+}
+
+void testFunc2(){
+    Serial.println("FOO!");
+
+    dsq.add_routine(0, 2, testFunc2);
+    dsq.add_routine(0, 5, testFunc3);
+}
+
+void setup(){
+    Serial.begin(9600);
+    delay(5000);
+    Serial.println("DSQ Test");
+  
+	  dsq.add_routine(0, 0, testFunc);
+    dsq.add_routine(0, 2, testFunc2);
+}
+
+void loop(){
+	dsq.execute();
+}
